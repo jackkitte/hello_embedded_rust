226287dfdf67a326db8fa5b5bc659541b2234536
jackkitte <jackkitte735@gmail.com>
2021-02-09 20:14:36 +0900

    Lチカを実装

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,29 @@
 extern crate panic_halt;
 
 use cortex_m_rt::entry;
-use cortex_m_semihosting::{debug, hprintln};
+use cortex_m_semihosting::debug;
+use stm32f4xx_hal::{delay::Delay, prelude::*, stm32};
 
 #[entry]
 fn main() -> ! {
-    let _ = hprintln!("Hello, world!");
+    if let (Some(dp), Some(cp)) = (stm32::Peripherals::take(), stm32::CorePeripherals::take()) {
+        let rcc = dp.RCC.constrain();
+        let clocks = rcc.cfgr.sysclk(48.mhz()).freeze();
+
+        let mut delay = Delay::new(cp.SYST, clocks);
+
+        let gpiod = dp.GPIOD.split();
+        let mut led = gpiod.pd14.into_push_pull_output();
+
+        for _ in 0..5 {
+            led.set_high().unwrap();
+            delay.delay_ms(100_u32);
+
+            led.set_low().unwrap();
+            delay.delay_ms(100_u32);
+        }
+    }
+
     debug::exit(debug::EXIT_SUCCESS);
 
     loop {}
